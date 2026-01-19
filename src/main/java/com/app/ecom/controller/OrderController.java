cb52c0ee4d3e47cefcc9fa9f9a458142cdcf85c9
Devansh27112001 <dkansara2711@gmail.com>
2026-01-18 23:49:37 -0500

    Working with Order Entity

---
+++ src/main/java/com/app/ecom/controller/OrderController.java
@@ -0,0 +1,14 @@
+package com.app.ecom.controller;
+
+import com.app.ecom.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+}
